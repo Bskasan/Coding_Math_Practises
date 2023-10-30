1ee3b4a136f866635bdc61d940b1403d950587d0
Bskasan <b.kasan@yandex.com>
2023-10-30 13:15:56 +0200

    up

--- Introduction/main.js
+++ Introduction/main.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   //? Access the Canvas Element
-  var canvas = document.getElementById(canvas),
+  var canvas = document.getElementById("canvas"),
     context = canvas.getContext("2d"),
     width = (canvas.width = window.innerWidth),
     height = (canvas.height = window.innerHeight);
