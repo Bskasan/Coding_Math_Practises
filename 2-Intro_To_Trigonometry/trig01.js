a430b4e90be2659271ae9789e07fe0df86dfccd9
Bskasan <b.kasan@yandex.com>
2023-10-31 16:32:19 +0200

    enough tri for today done

--- 2-Intro_To_Trigonometry/trig01.js
+++ 2-Intro_To_Trigonometry/trig01.js
@@ -5,6 +5,9 @@ window.onload = function () {
     width = (canvas.width = window.innerWidth),
     height = (canvas.height = window.innerHeight);
 
+  context.translate(0, height / 2);
+  context.scale(1, -1);
+
   for (var angle = 0; angle < Math.PI * 2; angle += 0.01) {
     var x = angle * 200,
       y = Math.sin(angle) * 200;
