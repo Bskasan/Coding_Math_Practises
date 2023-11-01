a1e1134fdd63f7c6f7c9cb52b6f9796aa38af293
Bskasan <b.kasan@yandex.com>
2023-11-01 22:56:57 +0200

    js and canvas added

---
+++ 3-More_Trigonometry/trig02.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  //? Access the Canvas Element
+  var canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d"),
+    width = (canvas.width = window.innerWidth),
+    height = (canvas.height = window.innerHeight);
+};
