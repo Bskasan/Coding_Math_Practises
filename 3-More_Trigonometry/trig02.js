978322d5a868aa27968a6f59814d889cc53db020
Bskasan <b.kasan@yandex.com>
2023-11-01 22:58:52 +0200

    up

--- 3-More_Trigonometry/trig02.js
+++ 3-More_Trigonometry/trig02.js
@@ -4,4 +4,13 @@ window.onload = function () {
     context = canvas.getContext("2d"),
     width = (canvas.width = window.innerWidth),
     height = (canvas.height = window.innerHeight);
+
+  // Define center position
+  var centerY = height * 0.5;
+  var centerX = width * 0.5;
+  var offSet = height * 0.4;
+  var speed = 0.1;
+  angle = 0;
+
+  // Render Function
 };
