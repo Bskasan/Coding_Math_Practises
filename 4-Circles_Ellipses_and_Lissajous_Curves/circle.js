080f58285c7dc239ee4ace74c200f210a1881029
Bskasan <b.kasan@yandex.com>
2023-11-02 22:24:03 +0200

    up

--- 4-Circles_Ellipses_and_Lissajous_Curves/circle.js
+++ 4-Circles_Ellipses_and_Lissajous_Curves/circle.js
@@ -7,9 +7,15 @@ window.onload = function () {
 
   var centerX = width / 2,
     centerY = height / 2,
-    radius = 200,
-    angle = 0,
-    speed = 0.1,
+    // radius = 200,
+    xRadius = 100,
+    yRadius = 300,
+    // angle = 0,
+    xAngle = 10,
+    yAngle = 10,
+    // speed = 0.1,
+    xSpeed = 0.01,
+    ySpeed = 0.131,
     x,
     y;
 
@@ -17,13 +23,19 @@ window.onload = function () {
 
   function render() {
     context.clearRect(0, 0, width, height);
-    x = centerX + Math.cos(angle) * radius;
-    y = centerY + Math.sin(angle) * radius;
+    x = centerX + Math.cos(xAngle) * xRadius;
+    y = centerY + Math.sin(yAngle) * yRadius;
 
     context.beginPath();
 
     //! The arc() method creates a circular arc centered at (x, y) with a radius of radius. The path starts at startAngle, ends at endAngle, and travels in the direction given by counterclockwise (defaulting to clockwise).
-    context.arc(x, y, 100, 0, Math.PI * 2, false);
+    context.arc(x, y, 25, 0, Math.PI * 2, false);
     context.fill();
+
+    // angle += speed;
+    xAngle += xSpeed;
+    yAngle += ySpeed;
+
+    requestAnimationFrame(render);
   }
 };
