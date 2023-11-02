94f332fd01f0636fbbe8c141307403cb98bcd039
Bskasan <b.kasan@yandex.com>
2023-11-02 22:35:03 +0200

    up

--- 4-Circles_Ellipses_and_Lissajous_Curves/circle.js
+++ 4-Circles_Ellipses_and_Lissajous_Curves/circle.js
@@ -7,33 +7,42 @@ window.onload = function () {
 
   var centerX = width / 2,
     centerY = height / 2,
-    // radius = 200,
-    xRadius = 100,
-    yRadius = 300,
-    // angle = 0,
-    xAngle = 10,
-    yAngle = 10,
-    // speed = 0.1,
-    xSpeed = 0.01,
-    ySpeed = 0.131,
+    radius = 200,
+    //xRadius = 100,
+    //yRadius = 300,
+    angle = 0,
+    //xAngle = 10,
+    //yAngle = 10,
+    //speed = 0.1,
+    //xSpeed = 0.01,
+    //ySpeed = 0.131,
     x,
     y;
 
-  function render() {
-    context.clearRect(0, 0, width, height);
-    x = centerX + Math.cos(xAngle) * xRadius;
-    y = centerY + Math.sin(yAngle) * yRadius;
+  x = centerX + Math.cos(angle) * radius;
+  y = centerY + Math.sin(angle) * radius;
+  context.beginPath();
+  context.arc(x, y, 10, 0, Math.PI * 2, false);
 
-    context.beginPath();
+  // render();
 
-    //! The arc() method creates a circular arc centered at (x, y) with a radius of radius. The path starts at startAngle, ends at endAngle, and travels in the direction given by counterclockwise (defaulting to clockwise).
-    context.arc(x, y, 25, 0, Math.PI * 2, false);
-    context.fill();
+  // Lissajous Curves
 
-    // angle += speed;
-    xAngle += xSpeed;
-    yAngle += ySpeed;
+  // function render() {
+  //   context.clearRect(0, 0, width, height);
+  //   x = centerX + Math.cos(xAngle) * xRadius;
+  //   y = centerY + Math.sin(yAngle) * yRadius;
 
-    requestAnimationFrame(render);
-  }
+  //   context.beginPath();
+
+  //   //! The arc() method creates a circular arc centered at (x, y) with a radius of radius. The path starts at startAngle, ends at endAngle, and travels in the direction given by counterclockwise (defaulting to clockwise).
+  //   context.arc(x, y, 25, 0, Math.PI * 2, false);
+  //   context.fill();
+
+  //   // angle += speed;
+  //   xAngle += xSpeed;
+  //   yAngle += ySpeed;
+
+  //   requestAnimationFrame(render);
+  // }
 };
